2bff3b169d2094d4f4b0fa12ce27a10878ec1498
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-03-19 23:28:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
