8db080583699ca5a377dd80283f1cf4df28585c5
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-03-25 23:26:51 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,18 @@
-const loginForm = document.querySelector('.login-form');
+const formEl = document.querySelector('.login-form');
 
-loginForm.addEventListener('submit', (event) => {
-event.preventDefault();
-const email = loginForm.elements.email.value;
-const password = loginForm.elements.password.value;
-
-if (!email || !password) {
-alert('All fields must be filled!');
-return;
-}
-
-const formData = {
-email,
-password,
-};
-
-console.log(formData);
-loginForm.reset();
-});
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password },
+    } = event.currentTarget;
+    const inputsEl = {
+        Email: `${email.value}`, 
+        Password: `${password.value}`
+    }
+    console.log(inputsEl);
+    
+    if (email.value === '' || password.value === '') {
+        window.alert('Всі поля повинні бути заповнені');
+    }
+    
+    event.target.reset();
+})
